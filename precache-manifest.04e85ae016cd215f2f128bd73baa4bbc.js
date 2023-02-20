54e4c90d83b606958e473fd65618d9691e67fd3c
GuillaumeAuger85 <auger.guillaume@gmail.com>
2023-02-20 16:01:08 -0500

    Updates

---
+++ precache-manifest.04e85ae016cd215f2f128bd73baa4bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59b95cc9e816508dc91f",
+    "url": "/React-Light-Out-Game/static/css/main.bc29200f.chunk.css"
+  },
+  {
+    "revision": "59b95cc9e816508dc91f",
+    "url": "/React-Light-Out-Game/static/js/main.59b95cc9.chunk.js"
+  },
+  {
+    "revision": "8ecd28d536744a3e8b39",
+    "url": "/React-Light-Out-Game/static/js/1.8ecd28d5.chunk.js"
+  },
+  {
+    "revision": "09cd8271998a50ff9238",
+    "url": "/React-Light-Out-Game/static/js/runtime~main.09cd8271.js"
+  },
+  {
+    "revision": "32da7e346c0118df2d3bce5a09f91672",
+    "url": "/React-Light-Out-Game/index.html"
+  }
+];
